66b3042f0b289a517ccf28c61fe6797433ba9b41
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2019-02-17 12:57:03 +0530

    webpage with js-freedom fighters

--- mai.js
+++ mai.js
@@ -1,18 +1,34 @@
 function loadDoc(filename) {
-    //();
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
        document.getElementById("qw").innerHTML = this.responseText;
-       history.pushState(null,null,"./"+filename)
       }
     };
-    xhttp.open("GET",filename , true);
+    xhttp.open("GET",filename,true);
     xhttp.send();
+    
+  }
+
+  function push(filename) {
+    history.pushState(null,null,filename)
+    
+  }
+
+  function asdf(filename) {
+    push("./"+filename);
+    loadDoc("./"+filename);
+    
   }
 
 
-
-  
-
-
+  window.onpopstate = function(event) {
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    loadDoc(page);
+    if(e.target != e.currentTarget){
+      e.preventDefault();
+      loadDoc(filename)
+		}
+      e.stopPropagation();
+	  }
